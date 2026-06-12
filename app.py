612b67e08ef7e38b5de6779be7bdbbb1612d962f
mcvals21 <31058974+mcvals21@users.noreply.github.com>
2026-06-12 14:59:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -209,4 +209,6 @@ def init_db():
         print(p.name, f'/p/{p.token}')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
