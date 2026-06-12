1d562024dca2448d632cd495180daf77bcd1a6e6
mcvals21 <31058974+mcvals21@users.noreply.github.com>
2026-06-12 17:09:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -213,10 +213,10 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
-        if Tournament.query.first() is None:
-            tournament = Tournament(name="كأس العالم 2026")
-            db.session.add(tournament)
-            db.session.commit()
+       if Tournament.query.first() is None:
+    new_tournament = Tournament(name="كأس العالم 2026")
+    db.session.add(new_tournament)
+    db.session.commit()
 
         if Participant.query.count() == 0:
             for name in PARTICIPANT_NAMES:
